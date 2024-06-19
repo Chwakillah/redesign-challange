cf0f3941ded7b84882183e9ef10d616f7dd53788
Chwakillah <syakillahnachwa081@gmail.com>
2024-06-19 20:12:37 +0700

    feat(hero): membuat hero section

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute("href");
+      const targetSection = document.querySelector(targetId);
+
+      if (targetSection) {
+        navLinks.forEach((link) => link.classList.remove("active"));
+        this.classList.add("active");
+        targetSection.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+});
